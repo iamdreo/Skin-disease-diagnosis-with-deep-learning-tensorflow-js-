9cf8ca83eb5b4f77697ffa7dac9db470c039adaa
DRE <andrew_ole@yahoo.com>
2018-09-10 11:56:48 +0100

    final comit

--- predict.js
+++ predict.js
@@ -14,7 +14,7 @@ $("#file").change(function () {
 
 let model;
 (async function() {
-    model = await tf.loadModel('http://localhost:8080/tfjs-models/model/model.json');
+    model = await tf.loadModel('https://glacial-river-37235.herokuapp.com/models/model/model.json');
     $('.progress-bar').hide();
 })();
 
